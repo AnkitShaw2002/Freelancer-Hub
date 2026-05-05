aff0f4fa8b78ca667cc12aeae6f9489f308a08b9
AnkitShaw2002 <ashaw2588@gmail.com>
2026-05-06 03:56:24 +0530

    ninth commit fix mail for render by brevo

--- app/config/emailConfig.js
+++ app/config/emailConfig.js
@@ -4,12 +4,13 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
     host: process.env.MAIL_HOST,
-    port: process.env.MAIL_PORT,
+    port: Number(process.env.MAIL_PORT),
     secure: false,
     auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS
-    } 
+    },
+    family: 4 //for render deploying 
 });
 
 module.exports = transporter;
